ef0fdcd0bc55f44da4bce97bcc37c0ef358f4ae8
Ali Rahmani <alirahmani.prog@gmail.com>
2023-05-17 23:13:58 +0330

    redesigned gallery page

--- src/app/component/project-card/project-card.component.ts
+++ src/app/component/project-card/project-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { defaultProject } from 'src/app/config/projects';
+import { Position } from 'src/app/core/type/position';
+import { getRandom } from 'src/app/core/util/random';
 import { Project } from 'src/app/type/gallery';
 
 @Component({
@@ -9,8 +11,34 @@ import { Project } from 'src/app/type/gallery';
 })
 export class ProjectCardComponent {
   @Input() project: Project = defaultProject;
+  mouse?: Position;
+  colors: string[] = [];
+
+  constructor() {
+    const color = getRandom(0, 261);
+
+    this.colors.push(`hsl(${Math.floor(color * 0.8)}deg 100% 50%)`);
+    this.colors.push(`hsl(${color}deg 100% 50%)`);
+    this.colors.push(`hsl(${Math.floor(color * 1.2)}deg 100% 50%)`);
+    console.log(this.colors);
+  }
 
   get category() {
     return this.project.category;
+  }
+
+  get hovered() {
+    return !!this.mouse;
+  }
+
+  onMouseMove(e: MouseEvent) {
+    this.mouse = {
+      x: e.offsetX,
+      y: e.offsetY,
+    };
+  }
+
+  onMouseLeave() {
+    this.mouse = undefined;
   }
 }
